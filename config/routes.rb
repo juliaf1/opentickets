2fef51b086c4377424f4fa73eb50f0d707897949
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-23 17:56:07 -0300

    Routes implemented for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :users, only: [ :index, :show, :edit, :update ]
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+  # get '/users/:id/edit', to: 'users#edit'
+  # patch '/users/:id/', to: 'users#update'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
