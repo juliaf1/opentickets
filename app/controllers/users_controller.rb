576d7a843b677d5c2623de43f1bf4db42427ae82
Rowan Douglas <rowan.douglas29@gmail.com>
2020-11-24 08:12:46 -0300

    added index, show, edit, update to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :find_user, only: [ :show ]
+  before_action :find_user, only: [ :show, :edit, :update ]
+  before_action :user_params, only: [ :update ]
 
   def index
     @users = User.all
@@ -9,9 +10,21 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    @user = @user.update(user_params)
+    @user.save
+  end
+
   private
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:hourly_rate)
   end
 end
