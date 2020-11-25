32f82c89abe1128d4172e242c5ef29a2ba4e542b
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-25 16:29:30 -0300

    added dependent destroy on models

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,6 +1,6 @@
 class Timeslot < ApplicationRecord
   belongs_to :user
-  has_one :ticket
+  has_one :ticket, dependent: :destroy
 
   validates :start_time, presence: true
   validates :start_time, uniqueness: { scope: :user_id }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :user_skills
-  has_many :timeslots
+  has_many :user_skills, dependent: :destroy
+  has_many :timeslots, dependent: :destroy
   has_many :skills, through: :user_skills
   has_many :tickets, through: :timeslots
   has_many :reviews, through: :tickets
