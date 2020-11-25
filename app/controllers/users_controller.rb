c4a8c4785130f2e77614d726bd0d48a98bd62ed9
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-25 19:14:12 -0300

    taken out unnescessary params from strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
     filtered_timeslots = Timeslot.where('start_time BETWEEN ? AND ?', params[:beginning], params[:end])
+
     available_filtered_timeslots = filtered_timeslots.reject do |timeslot|
       timeslot.ticket
     end
+
     @filtered_teachers = available_filtered_timeslots.map do |timeslot|
       timeslot.user
     end.uniq
@@ -36,6 +39,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :bio, :photo, :hourly_rate, :beginning, :end)
+    params.require(:user).permit(:first_name, :last_name, :bio, :photo, :hourly_rate)
   end
 end
