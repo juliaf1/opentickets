bb2bf0e66391ba67a44579eb7ad2a9a84fcc47ce
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-25 12:33:16 -0300

    cloudinary integration set up for development and production enviroment + user show, user index and user edit updated to integrate with avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :bio, :hourly_rate)
+    params.require(:user).permit(:first_name, :last_name, :email, :bio, :photo, :hourly_rate)
   end
 end
