ddeba4067fec54d060479a25423d9636f72a5d25
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-25 12:56:13 -0300

    avatar photos implemented on seeds bug fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,6 @@ puts '*** Creating some beautiful users ***'
 
 lewagon_teachers = []
 
-matheus_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U3N8S3LFK-73039713a064-512")
 matheus = {
   first_name: 'Matheus',
   last_name: 'Penchel',
@@ -24,10 +23,8 @@ matheus = {
   hourly_rate: 100,
   bio: 'The Godfather of Le Wagon Rio, master of the Mastadon and King of the Shrubbles. I can teach you almost anything you need to know.'
   }
-matheus.photo.attach(io: matheus_avatar, filename: 'matheus.jpeg', content_type: 'image/jpeg')
 lewagon_teachers << matheus
 
-ciro_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-USL030V24-5c51862252d8-512")
 ciro = {
   first_name: 'Ciro',
   last_name: 'Lima',
@@ -36,10 +33,8 @@ ciro = {
   hourly_rate: 50,
   bio: 'Master of APIs. I can help you with any problem you have!'
   }
-ciro.photo.attach(io: ciro_avatar, filename: 'ciro.jpeg', content_type: 'image/jpeg')
 lewagon_teachers << ciro
 
-tatchi_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-USJJY7ZUG-b163bf314111-512")
 tatchi = {
   first_name: 'Tatchi',
   last_name: 'Wiggers',
@@ -48,10 +43,8 @@ tatchi = {
   hourly_rate: 50,
   bio: "Queen of JavaScript, master of the wiggers. I have the coolest hair in Le Wagon and there's nothing you can do about it."
   }
-tatchi.photo.attach(io: tatchi_avatar, filename: 'tatchi.jpeg', content_type: 'image/jpeg')
 lewagon_teachers << tatchi
 
-pedro_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-US7288YNP-adabe5cee822-512")
 pedro = {
   first_name: 'Pedro',
   last_name: 'Miranda',
@@ -60,10 +53,8 @@ pedro = {
   hourly_rate: 5,
   bio: "Quero jogar futebol contigo."
   }
-pedro.photo.attach(io: pedro_avatar, filename: 'pedro.jpeg', content_type: 'image/jpeg')
 lewagon_teachers << pedro
 
-milene_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-UDE76BKKN-1936f122810b-512")
 milene = {
   first_name: 'Milene',
   last_name: 'Cardoso',
@@ -72,10 +63,8 @@ milene = {
   hourly_rate: 1,
   bio: "I have a 5 year plan for me and for all of you. Let's Marie Kondo your life to be super productive!"
   }
-milene.photo.attach(io: milene_avatar, filename: 'milene.jpeg', content_type: 'image/jpeg')
 lewagon_teachers << milene
 
-marcel_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-UDAKDMC5U-22cee4862fb3-512")
 marcel = {
   first_name: 'Marcel',
   last_name: 'Fonseca',
@@ -84,15 +73,41 @@ marcel = {
   hourly_rate: 10,
   bio: "Learn some really cool stuff with me and my crazy cats. I love to code and I love to teach!"
   }
-marcel.photo.attach(io: marcel_avatar, filename: 'marcel.jpeg', content_type: 'image/jpeg')
 lewagon_teachers << marcel
 
+# Iterating over array of teachers hashes to create users
 lewagon_teachers.each do |teacher|
   User.create(teacher)
   puts "Created #{teacher[:first_name]}"
 end
 
+# Finding teachers instances
+matheus = User.find(1)
+ciro = User.find(2)
+tatchi = User.find(3)
+pedro = User.find(4)
+milene = User.find(5)
+marcel = User.find(6)
+
+# Defining teachers avatars
+matheus_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U3N8S3LFK-73039713a064-512")
+ciro_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-USL030V24-5c51862252d8-512")
+tatchi_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-USJJY7ZUG-b163bf314111-512")
+pedro_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-US7288YNP-adabe5cee822-512")
+milene_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-UDE76BKKN-1936f122810b-512")
+marcel_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-UDAKDMC5U-22cee4862fb3-512")
+
+# Assigning avatars to teachers
+matheus.photo.attach(io: matheus_avatar, filename: 'matheus.jpeg', content_type: 'image/jpeg')
+ciro.photo.attach(io: ciro_avatar, filename: 'ciro.jpeg', content_type: 'image/jpeg')
+tatchi.photo.attach(io: tatchi_avatar, filename: 'tatchi.jpeg', content_type: 'image/jpeg')
+pedro.photo.attach(io: pedro_avatar, filename: 'pedro.jpeg', content_type: 'image/jpeg')
+milene.photo.attach(io: milene_avatar, filename: 'milene.jpeg', content_type: 'image/jpeg')
+marcel.photo.attach(io: marcel_avatar, filename: 'marcel.jpeg', content_type: 'image/jpeg')
+
+
 puts '*** User Seeding Complete ***'
+
 puts '*** Creating some User Skills ***'
 
 teachers = User.all
@@ -111,7 +126,6 @@ puts '*** Creating some User Students ***'
 
 students = []
 
-kenneth_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01CLNSQXBN-77d374596e5f-512")
 kenneth = {
   first_name: 'Kenneth',
   last_name: 'Wall',
@@ -119,10 +133,8 @@ kenneth = {
   password: '123456',
   bio: "USA, USA, USA! Trump sucks!"
   }
-kenneth.photo.attach(io: kenneth_avatar, filename: 'kenneth.jpeg', content_type: 'image/jpeg')
 students << kenneth
 
-filipe_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01CELLS6G2-013b848074c6-512")
 filipe = {
   first_name: 'Filipe',
   last_name: 'Alencar',
@@ -130,10 +142,8 @@ filipe = {
   password: '123456',
   bio: "JavaScript is Love."
   }
-filipe.photo.attach(io: filipe_avatar, filename: 'filipe.jpeg', content_type: 'image/jpeg')
 students << filipe
 
-gabriel_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01D4HY364Q-acc38bde08a6-512")
 gabriel = {
   first_name: 'Gabriel',
   last_name: 'Ferro',
@@ -141,10 +151,8 @@ gabriel = {
   password: '123456',
   bio: "Lady Gaga is my idol."
   }
-gabriel.photo.attach(io: gabriel_avatar, filename: 'gabriel.jpeg', content_type: 'image/jpeg')
 students << gabriel
 
-cynthia_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01BZSP6T1V-1ab6ccfc08af-512")
 cynthia = {
   first_name: 'Cynthia',
   last_name: 'Tsai',
@@ -152,7 +160,6 @@ cynthia = {
   password: '123456',
   bio: "Drums & Code!"
   }
-cynthia.photo.attach(io: cynthia_avatar, filename: 'cynthia.jpeg', content_type: 'image/jpeg')
 students << cynthia
 
 students.each do |student|
@@ -161,6 +168,24 @@ students.each do |student|
 end
 
 puts '*** Students Created ***'
+
+# Finding students instances
+kenneth = User.find(7)
+filipe = User.find(8)
+gabriel = User.find(9)
+cynthia = User.find(10)
+
+# Defining students avatars
+kenneth_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01CLNSQXBN-77d374596e5f-512")
+filipe_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01CELLS6G2-013b848074c6-512")
+gabriel_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01D4HY364Q-acc38bde08a6-512")
+cynthia_avatar = URI.open("https://ca.slack-edge.com/T02NE0241-U01BZSP6T1V-1ab6ccfc08af-512")
+
+#Assigning avatars to students
+kenneth.photo.attach(io: kenneth_avatar, filename: 'kenneth.jpeg', content_type: 'image/jpeg')
+filipe.photo.attach(io: filipe_avatar, filename: 'filipe.jpeg', content_type: 'image/jpeg')
+gabriel.photo.attach(io: gabriel_avatar, filename: 'gabriel.jpeg', content_type: 'image/jpeg')
+cynthia.photo.attach(io: cynthia_avatar, filename: 'cynthia.jpeg', content_type: 'image/jpeg')
 
 puts '*** Creating Teacher Timeslots ***'
 
