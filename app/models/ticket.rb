6283706d64d4556a26213ef8ee7309f93f777b1c
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-25 16:41:25 -0300

    add association dependent destroy for reviews

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :timeslot
   belongs_to :user
-  has_one :review
+  has_one :review, dependent: :destroy
 
   validates :timeslot_id, uniqueness: true
 end
