a34a5b8b68f8128d93214de3d5ad3dc523657c92
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-25 09:10:35 -0300

    add a review button won't appear if you already left a review for that ticket. review model validates ticket uniquenes, only possible to create one review per ticket. missing error mesages

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,5 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true, less_than: 6 }
   validates :content, length: { in: 12..500 }
+  validates :ticket_id, uniqueness: true
 end
