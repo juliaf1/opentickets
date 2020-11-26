3b173ff81f8f68ce87ce00f03c0ade46e960d7ec
Julia Frederico <magalhaesjulia@id.uff.br>
2020-11-26 00:30:59 -0300

    wip - geocoder gem added, runned migrations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :user_skills, dependent: :destroy
   has_many :timeslots, dependent: :destroy
   has_many :skills, through: :user_skills
